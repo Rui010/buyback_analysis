10456c54435fa2226e75b34fe25897e691592844
Rui Ito <ito.rui.521@gmail.com>
2025-06-10 21:27:47 +0900

    repair detecttype!

---
+++ buyback_analysis/models/correction.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Date, Float, BigInteger
+from buyback_analysis.models.base import Base
+
+
+class Correction(Base):
+    __tablename__ = "correction"
+
+    code = Column(String, primary_key=True)
+    disclosure_date = Column(String, primary_key=True)
+    url = Column(String)
+    company_name = Column(String)
+    start_date = Column(String)
+    end_date = Column(String)
+    shares_acquired = Column(Float)
+    amount_spent_yen = Column(BigInteger)
+    buyback_method = Column(BigInteger)
