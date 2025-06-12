cf183f63e504c85759f59983e5250c59eb251040
Rui Ito <ito.rui.521@gmail.com>
2025-06-12 14:48:42 +0900

    migration

--- buyback_analysis/models/correction.py
+++ buyback_analysis/models/correction.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, Date, Text, ForeignKey, JSON
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, Date, Text, JSON
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
@@ -9,9 +9,10 @@ class Correction(Base):
 
     id = Column(Integer, primary_key=True)
     code = Column(String, nullable=False)
+    url = Column(String)
     company_name = Column(String, nullable=False)
-    disclosure_date = Column(Date, nullable=False)
-    original_announcement_date = Column(Date, nullable=False)
+    disclosure_date = Column(String, nullable=False)
+    original_announcement_date = Column(String, nullable=False)
     document_title = Column(String, nullable=False)
     correction_reason = Column(Text, nullable=True)
-    corrections_json = Column(JSON, nullable=False)
+    corrections = Column(JSON, nullable=False)
--- buyback_analysis/usecase/data_exists.py
+++ buyback_analysis/usecase/data_exists.py
@@ -3,6 +3,7 @@ from buyback_analysis.models.announcement import Announcement
 from buyback_analysis.models.progress import Progress
 from buyback_analysis.models.completion import Completion
 from buyback_analysis.models.is_checked import IsChecked
+from buyback_analysis.models.correction import Correction
 from buyback_analysis.usecase.logger import Logger
 
 
@@ -29,6 +30,9 @@ def data_exists_in_ir_tables(session: Session, url: str) -> bool:
             return True
         # Completion テーブルで URL を検索
         if session.query(Completion).filter(Completion.url == url).first():
+            return True
+        # Correction テーブルで URL を検索
+        if session.query(Correction).filter(Correction.url == url).first():
             return True
         return False
     except Exception as e:
